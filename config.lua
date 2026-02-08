6e4fd99b75d2d5f89134ba6f21bfe0e555729e04
SEYA-CODING <seyattv@gmail.com>
2026-02-08 23:35:24 +0000

    Add initial configuration settings in config.lua

---
+++ config.lua
@@ -0,0 +1,15 @@
+Config = {}
+
+Config.Framework = 'auto' -- auto | qb | esx
+
+Config.AdminGroups = {
+    'admin',
+    'superadmin',
+    'god'
+}
+
+Config.InstallTime = 7000 -- milliseconds
+
+Config.TabletCommand = 'admingps'
+
+Config.GPSItemName = 'gps_device' -- optional future use
